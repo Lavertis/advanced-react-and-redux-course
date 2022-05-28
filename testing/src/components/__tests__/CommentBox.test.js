2435e9568e0516daece37ea244a3b6f7ec75fe64
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-28 22:04:32 +0200

    34. AfterEach Statements

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -2,12 +2,17 @@ import React from "react";
 import {mount} from "enzyme";
 import CommentBox from "components/CommentBox";
 
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox/>);
+});
+
+afterEach(() => {
+    wrapped.unmount();
+});
+
 it('has a text area and a button', () => {
-    const wrapped = mount(<CommentBox/>);
-
-    console.log(wrapped.find('textarea'));
-    console.log(wrapped.find('button'));
-
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
