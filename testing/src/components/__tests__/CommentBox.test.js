99f1ee6297ad501b0ffc20599002caa2a8214a4b
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-28 22:22:43 +0200

    36. Providing Fake Events

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,11 @@ it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that user can type in', () => {
+    const text = 'new comment';
+    wrapped.find('textarea').simulate('change', {
+        target: {value: text}
+    });
+    expect(wrapped.find('textarea').prop('value')).toEqual(text);
+});
