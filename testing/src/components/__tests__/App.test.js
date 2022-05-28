a4e8d6b5bba9b3105b7552ea5640c36d9403aac8
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-28 20:52:11 +0200

    24. Expecting the Comment List

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,8 +1,14 @@
 import {shallow} from 'enzyme';
 import App from '../App';
 import CommentBox from "../CommentBox";
+import CommentList from "../CommentList";
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App/>);
-  expect(wrapped.find(CommentBox).length).toEqual(1);
+    const wrapped = shallow(<App/>);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+    const wrapped = shallow(<App/>);
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
