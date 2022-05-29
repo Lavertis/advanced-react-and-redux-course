cbc14911dfd92f11925955335d6ae64e2bb38217
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-29 20:38:45 +0200

    56. Fetching a Remote Resource

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -1,14 +1,17 @@
 import {Provider} from "react-redux";
 import React from "react";
-import {createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import reducers from "./reducers";
-
+import reduxPromise from "redux-promise";
 
 const Root = ({children, initialState = {}}) => {
     const store = createStore(
         reducers,
         initialState,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        compose(
+            applyMiddleware(reduxPromise),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
     )
 
     return (
--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,8 +1,18 @@
-import {SAVE_COMMENT} from "./types";
+import {FETCH_COMMENTS, SAVE_COMMENT} from "./types";
+import axios from "axios";
 
 export const saveComment = (comment) => {
     return {
         type: SAVE_COMMENT,
         payload: comment
+    }
+}
+
+export const fetchComments = () => {
+    const response = axios.get('https://jsonplaceholder.typicode.com/comments')
+
+    return {
+        type: FETCH_COMMENTS,
+        payload: response
     }
 }
