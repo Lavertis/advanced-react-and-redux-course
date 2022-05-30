9c65049f86abec3b135e5fdd9850cb56f03e20c5
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-31 00:00:27 +0200

    84. How Async Middlewares Work

---
+++ middlewares-with-redux/src/components/CommentBox.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import * as actions from '../actions';
+import requireAuth from "./requireAuth";
+
+const CommentBox = () => {
+    const dispatch = useDispatch();
+    const [comment, setComment] = useState('');
+
+    const handleChange = (event) => {
+        setComment(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(actions.saveComment(comment));
+        setComment('');
+    }
+
+    return (
+        <div data-testid="CommentBox">
+            <form onSubmit={handleSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea
+                    value={comment}
+                    onChange={handleChange}
+                />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+            <button id='fetch-comments-button' onClick={() => dispatch(actions.fetchComments())}>
+                Fetch Comments
+            </button>
+        </div>
+    );
+}
+
+CommentBox.propTypes = {};
+
+CommentBox.defaultProps = {};
+
+export default requireAuth(CommentBox);
