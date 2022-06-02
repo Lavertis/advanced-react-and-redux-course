52634ed17a0bd812e303164f888d835869c8529e
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-02 17:29:17 +0200

    103. Inspecting the Database

--- authentication/backend/app.js
+++ authentication/backend/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
+const router = require('./router');
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/auth');
 
 // App Setup
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+router(app)
 
 // Server Setup
 const port = process.env.PORT || '3090';
---
+++ authentication/backend/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    password: String
+});
+
+const model = mongoose.model('User', userSchema);
+module.exports = model;
